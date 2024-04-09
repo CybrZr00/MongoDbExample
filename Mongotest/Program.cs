076fe664fcb8b5ae65c5afb78dbd26a10df6d2da
Mark <markperry.uk@gmail.com>
2024-04-10 00:02:17 +0100

    add serilog

--- Mongotest/Program.cs
+++ Mongotest/Program.cs
@@ -1,10 +1,27 @@
 using Mongotest.Data;
 
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+var log = new LoggerConfiguration()
+    .WriteTo.MongoDBBson(
+    databaseUrl: "mongodb://localhost:27017/mongotest_v1", 
+    collectionName: "logs", 
+    rollingInterval: Serilog.Sinks.MongoDB.RollingInterval.Day, 
+    cappedMaxSizeMb: 2048)
+    .MinimumLevel.Information()
+    .Enrich.FromLogContext()
+    .CreateLogger();
+    
+
+// Register the logger as a singleton
+builder.Services.AddSingleton<Serilog.ILogger>(log);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,7 +34,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();

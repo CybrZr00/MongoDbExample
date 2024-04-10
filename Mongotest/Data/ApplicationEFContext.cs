3dc2d3edd2d727d995e5763f5699a57b6ba7115f
Mark <markperry.uk@gmail.com>
2024-04-10 22:32:58 +0100

    Added EF Auditing

--- Mongotest/Data/ApplicationEFContext.cs
+++ Mongotest/Data/ApplicationEFContext.cs
@@ -11,5 +11,41 @@ namespace Mongotest.Data
         {
                
         }
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            // Get all the entities that inherit from BaseModel
+            // and have a state of Added or Modified
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is BaseModel && (
+                        e.State == EntityState.Added
+                        || e.State == EntityState.Modified));
+
+            // For each entity we will set the Audit properties
+            foreach (var entityEntry in entries)
+            {
+                // If the entity state is Added let's set
+                // the DateCreated property to UtcNow
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((BaseModel)entityEntry.Entity).DateCreated = DateTime.UtcNow;
+                }
+                else
+                {
+                    // If the state is Modified then we don't want
+                    // to modify the DateCreated property
+                    // so we set its state as IsModified to false
+                    Entry((BaseModel)entityEntry.Entity).Property(p => p.DateCreated).IsModified = false;
+                }
+
+                // In any case we always want to set the
+                // DateLastUpdated
+                ((BaseModel)entityEntry.Entity).DateLastUpdated = DateTime.UtcNow;
+            }
+            // After we set all the needed properties
+            // we call the base implementation of SaveChangesAsync
+            // to actually save our entities in the database
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
